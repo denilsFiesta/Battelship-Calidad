748a29bbd46e3a96b6fd6ab6710e8fe35b039fc8
Al1bi <denilson.fiesta@ucb.edu.bo>
2024-10-12 17:47:26 -0400

    Pruebas de prueba para la clase BattleshipController

--- test/battleshipTest/controllerTest/BattleshipControllerTest.java
+++ test/battleshipTest/controllerTest/BattleshipControllerTest.java
@@ -11,8 +11,26 @@ import battleship.controller.BattleshipController;
 public class BattleshipControllerTest {
 
     @Test
-    public void testRequestForUserInput(){
+    public void testRequestForUserInput_correctInput(){
         Scanner scannerSimulado = new Scanner("1\n"); 
+        BattleshipController battleshipController = new BattleshipController(scannerSimulado);
+
+        int result = battleshipController.requestForUserInput("", 1, 2);
+        assertEquals("La entra 1 es valida en el rango [1,2]", 1, result);
+    }
+
+    @Test
+    public void testRequestForUserInput_invalidInput(){
+        Scanner scannerSimulado = new Scanner("a 1\n"); 
+        BattleshipController battleshipController = new BattleshipController(scannerSimulado);
+
+        int result = battleshipController.requestForUserInput("", 1, 2);
+        assertEquals("La entra a no es valida en el rango [1,2]", 1, result);
+    }
+
+    @Test
+    public void testRequestForUserInput_outOfRangeInput(){
+        Scanner scannerSimulado = new Scanner("7 1\n"); 
         BattleshipController battleshipController = new BattleshipController(scannerSimulado);
 
         int result = battleshipController.requestForUserInput("", 1, 2);
