34200a940ff2430ee003bb0a3b3f55e5e751a0b1
Al1bi <denilson.fiesta@ucb.edu.bo>
2024-10-12 14:28:22 -0400

    prueba fuera de contexto agregada

---
+++ src/battleship/Calculator.java
@@ -0,0 +1,11 @@
+package battleship;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int mayor(int a, int b){
+        if(a > b) return a;
+        else return b;
+    }
+}
