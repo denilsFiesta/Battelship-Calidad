dd6b5b7aaccdad9f2ea7ab58d43eda99d5bdcfe6
Jhoselin Quiroga <jhoselin.quiroga@ucb.edu.bo>
2024-10-12 21:09:53 -0400

    Crear test que asegura lanzamiento de IllegalArgumentException en Ocean con tamaño no válido

--- src/test/java/com/modelTest/gameTest/OceanTest.java
+++ src/test/java/com/modelTest/gameTest/OceanTest.java
@@ -1,11 +1,20 @@
 package com.modelTest.gameTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 import com.model.game.ocean.Ocean;
 public class OceanTest {
+
+    @Test
+    public void testOceanCreation_invalidHorizontalAndVerticalSize() {
+        // Verificamos que se lanza IllegalArgumentException al intentar crear Ocean con tamaño inválido
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Ocean(0, 0); // Ambos tamaños fuera del rango permitido
+        });
+    }
 
     @Test
     public void testOceanCreation_validSize() {
