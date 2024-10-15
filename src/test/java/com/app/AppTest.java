8da93d7250d61c4a54b73f282a5655703eab81b2
AdaiIllanesGutierrez <adai.illanes@ucb.edu.bo>
2024-10-15 10:21:52 -0400

    Clase App, funcion main, refactorizacion de la prueba para argumentos validos

--- src/test/java/com/app/AppTest.java
+++ src/test/java/com/app/AppTest.java
@@ -5,6 +5,8 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 import org.junit.jupiter.api.Test;
@@ -12,6 +14,7 @@ import org.mockito.MockedConstruction;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
+import com.CustomInputStream;
 import com.app.App;
 import com.controller.BattleshipController;
 
@@ -27,11 +30,13 @@ public class AppTest {
 
     @Test
     public void testMain_SevenArgs() {
-        String args[] = {"10", "10", "5", "5", "5", "5", "5"};
-        
-        //Recalcar que el programa termina en una excepcion debido a que dentro existen funcion que depende de la entrada del usuario
-        try (MockedConstruction<BattleshipController> mockedConstruction = mockConstruction(BattleshipController.class)) {
-            App.main(args);
-        }
+        String args[] = {"10", "10", "0", "0", "0", "0", "0"};
+        List<String> inputs = Arrays.asList(
+            "1 0 1\n",
+            "0 0\n"
+        );
+        CustomInputStream customInputStream = new CustomInputStream(inputs);
+        System.setIn(customInputStream);
+        App.main(args);
     }
 }
