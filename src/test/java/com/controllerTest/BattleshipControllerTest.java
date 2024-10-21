76a6fa5815ee9a61582651ec9cbf681946c4b50e
denilsFiesta <denilson.fiesta@ucb.edu.bo>
2024-10-20 20:51:20 -0400

    Class BattleController, Method requestForRecoveryMode, caso que el juego se inicia con el modo recovery desactivado

--- src/test/java/com/controllerTest/BattleshipControllerTest.java
+++ src/test/java/com/controllerTest/BattleshipControllerTest.java
@@ -384,4 +384,14 @@ public class BattleshipControllerTest {
         BattleshipController battleshipController = new BattleshipController();
         battleshipController.startGame(args);
     }
+
+    @Test
+    public void testRequestForRecoveryMode_ModeDisabled(){
+        int args[] = {1, 1, 0, 0, 0, 0, 0};
+        List<String> inputs = Arrays.asList("2 2\n");
+        setInputs(inputs);
+        
+        BattleshipController battleshipController = new BattleshipController();
+        battleshipController.startGame(args);
+    }
 }
